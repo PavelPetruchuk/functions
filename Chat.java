97d422da9e5ed609413d91b3d5efb979a9e815a3
PavelPetruchuk <91282971+PavelPetruchuk@users.noreply.github.com>
2022-06-11 13:31:31 +0300

    Add files via upload

---
+++ Chat.java
@@ -0,0 +1,24 @@
+package oop.colections.hw1;
+
+public class Chat {
+    private final String name;
+    private final int numberOfUsers;
+
+    public Chat(String name, int numberOfUsers) {
+        this.name = name;
+        this.numberOfUsers = numberOfUsers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfUsers() {
+        return numberOfUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "Chat{" + "name='" + name + '\'' + ", numberOfUsers=" + numberOfUsers + '}';
+    }
+}
